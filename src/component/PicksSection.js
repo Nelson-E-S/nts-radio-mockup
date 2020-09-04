a7f9ce6d6dabdb750dd3fa3cbeba5bcc14429696
Nelson-E-S <nelson.e.sanabria1@gmail.com>
2020-09-04 15:31:05 -0400

    nts picks 1st draft completed

--- src/component/PicksSection.js
+++ src/component/PicksSection.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import PicksCard from './PicksCard';
 import {picksData} from '../data/picksData';
+import '../styles/PicksSection.css'
 const PicksSection = () =>(
     <section id="nts_pics">
         <h4>NTS Picks</h4>
-        {picksData.map((item,index)=><PicksCard key={index} date={item.date} loc={item.loc} title={item.title} tags={item.tags} src={item.img} />)}
+        <div className="body">
+            {picksData.map((item,index)=><PicksCard key={index} date={item.date} loc={item.loc} title={item.title} tags={item.tags} src={item.img} />)}
+        </div>
     </section>
 );
 
