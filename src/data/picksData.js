c0483d567a7963cca60604460544a60d89920787
Nelson-E-S <nelson.e.sanabria1@gmail.com>
2020-09-04 16:36:20 -0400

    minor edits to demo

--- src/data/picksData.js
+++ src/data/picksData.js
@@ -41,4 +41,18 @@ export const picksData = [
         img: 'https://via.placeholder.com/200x100/FFFF00/000000?text=',
         tags: ['TECHNO','BREAKS']
     },
+    {
+        date: '15.12.19',
+        loc: 'LON',
+        title: 'RE:LAX W/ RE:NI & LAKSA, GIGSTA & MINOR SCIENCE',
+        img: 'https://via.placeholder.com/200x100/FFFF00/000000?text=',
+        tags: ['TECHNO','BREAKS']
+    },
+    {
+        date: '15.12.19',
+        loc: 'LON',
+        title: 'RE:LAX W/ RE:NI & LAKSA, GIGSTA & MINOR SCIENCE',
+        img: 'https://via.placeholder.com/200x100/FFFF00/000000?text=',
+        tags: ['TECHNO','BREAKS']
+    },
 ]
