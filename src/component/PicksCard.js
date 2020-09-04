14a42231c454386066316122298caf52ad089100
Nelson-E-S <nelson.e.sanabria1@gmail.com>
2020-09-04 15:06:15 -0400

    picksCard styling added

--- src/component/PicksCard.js
+++ src/component/PicksCard.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import PicksTags from './PicksTags';
+import '../styles/PicksCard.css';
 
 const PicksCard = (props) =>(
     <div className="picksCard">
-        <img src="#" alt="#" />
-        <div className="cardDate">
-            <span>{props.date}</span>
-            <span>{props.loc?props.loc:null}</span>
-        </div>
-        <span className="title">
-            {props.title}
-        </span>
-        <div className="cardTags">
-            {props.tags.map((item, index)=><PicksTags name={item} key={index}/>)}
+        <div className="picksCardContent">
+            <img src={`${props.src}${props.title.replace(/\s/g,'+').replace(/&/g,' and ')}`} alt={props.title} />
+            <div className="cardDate">
+                <span>{props.date}</span>
+                <span>{props.loc?props.loc:null}</span>
+            </div>
+            <span className="title">
+                {props.title}
+            </span>
+            <div className="cardTags">
+                {props.tags.map((item, index)=><PicksTags name={item} key={index}/>)}
+            </div>
         </div>
     </div>
 );
--- src/component/PicksSection.js
+++ src/component/PicksSection.js
@@ -4,7 +4,7 @@ import {picksData} from '../data/picksData';
 const PicksSection = () =>(
     <section id="nts_pics">
         <h4>NTS Picks</h4>
-        {picksData.map((item,index)=><PicksCard key={index} date={item.date} loc={item.loc} title={item.title} tags={item.tags} />)}
+        {picksData.map((item,index)=><PicksCard key={index} date={item.date} loc={item.loc} title={item.title} tags={item.tags} src={item.img} />)}
     </section>
 );
 
